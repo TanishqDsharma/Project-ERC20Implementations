4104460d16ff942a1204b78293498a3567bc5f77
Tanishq <43058749+TanishqDsharma@users.noreply.github.com>
2025-05-21 17:21:05 +0530

    Update ERC20Implementation1.t.sol

--- Test/ERC20Implementation1.t.sol
+++ Test/ERC20Implementation1.t.sol
@@ -1,1 +1,39 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.20;
 
+
+import "../lib/forge-std/src/Test.sol";
+import {ERC20BuggyImplementation1} from "../src/ERC20Implementation1.sol";
+
+
+contract TestERC20Implementation1 is Test {
+
+ERC20BuggyImplementation1 erc20Implementation1;
+address user = makeAddr("user");
+
+    function setUp() external{
+        erc20Implementation1 = new ERC20BuggyImplementation1("Buggy","BUGGY20");
+    }
+
+    function testGetTokenName() public {
+        assertEq(erc20Implementation1.name(),"Buggy");
+    }
+
+    function testGetTokenSymbol() public {
+        assertEq(erc20Implementation1.symbol(),"BUGGY20");
+    }
+
+    function testGetTokenDecimals() public{
+        assertEq(erc20Implementation1.decimals(),18);
+    }
+
+    function testTotalSupply() public{
+        assertEq(erc20Implementation1.totalSupply(),0);
+    }
+
+    function testGetTokenBalance() public{
+        assertEq(erc20Implementation1.balanceOf(address(user)),0);
+    }
+
+    
+}
